72b41ca2bcf8911d8355145e94bd369b9b8ff15d
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-03-02 17:02:29 -0800

    03/02/2021

--- src/_07_California_Weather/CaliforniaWeather.java
+++ src/_07_California_Weather/CaliforniaWeather.java
@@ -1,6 +1,10 @@
 package _07_California_Weather;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.swing.JOptionPane;
 
 /*
  * OBJECTIVE:
@@ -9,7 +13,7 @@ import java.util.HashMap;
  * and the Utilities class inside this project to get the temperature data
  * from a day in December 2020.
  * Example: User: Encinitas
- *          Program: Encinitas is Overcast with a tempeature of 59.01 °F
+ *          Program: Encinitas is Overcast with a tempeature of 59.01 ï¿½F
  * 
  * 2. Create a way for the user to specify the weather condition and then
  * list the cities that have those conditions.
@@ -18,7 +22,7 @@ import java.util.HashMap;
  * 
  * 3. Create a way for the user to enter a minimum and maximum temperature
  * and then list the cities that have temperatures within that range
- * Example: User: minimum temperature °F = 65.0, max temperature °F = 70.0
+ * Example: User: minimum temperature ï¿½F = 65.0, max temperature ï¿½F = 70.0
  *          Program: Fortana, Glendale, Escondido, Del Mar, ...
  *          
  * EXTRA:
@@ -31,15 +35,49 @@ public class CaliforniaWeather {
     
     void start() {
         HashMap<String, WeatherData> weatherData = Utilities.getWeatherData();
-        
+        	
         // All city keys have the first letter capitalized of each word
-        String cityName = Utilities.capitalizeWords( "National City" );
-        WeatherData datum = weatherData.get(cityName);
-        
-        if( datum == null ) {
-            System.out.println("Unable to find weather data for: " + cityName);
-        } else {
-            System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
+        String options[] = {"Get the weather in a city", "Find cities with a weather condition", "Find cities with a minimum and maximum temperature"};
+        int choice = JOptionPane.showOptionDialog(null, "Choose an Option", null, 1, 1, null, options,0);
+        if (choice == 0) {
+            String cityName = Utilities.capitalizeWords(JOptionPane.showInputDialog("Enter a city"));
+            WeatherData datum = weatherData.get(cityName);
+            if (datum == null) {
+                JOptionPane.showMessageDialog(null, "Unable to find weather data for: " + cityName);
+            } else {
+            	DecimalFormat df = new DecimalFormat("#.##");
+            	double celsius = Double.parseDouble(df.format((datum.temperatureF-32)*0.5556));
+            	JOptionPane.showMessageDialog(null, cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + "Â°F or " + celsius + "Â°C");
+            }
+        }
+        else if (choice == 1) {
+            String weatherCondition = Utilities.capitalizeWords(JOptionPane.showInputDialog("Enter a weather condition (Mostly Cloudy, Clear, Overcast, Partly Cloudy)"));
+            ArrayList<String> data = new ArrayList<>();
+            for (String w : weatherData.keySet()) {
+            	if (weatherData.get(w).weatherSummary.equals(weatherCondition)) {
+            		data.add(w);
+            	}
+            }
+            String msg = "Cities with Weather Condition " + weatherCondition + ": ";
+            for (String d : data) {
+            	msg += d + ", ";
+            }
+            JOptionPane.showMessageDialog(null, msg);
+        }
+        else if (choice == 2) {
+        	double min = Double.parseDouble(JOptionPane.showInputDialog("Enter the minimum value"));
+        	double max = Double.parseDouble(JOptionPane.showInputDialog("Enter the maximum value"));
+        	ArrayList<String> data = new ArrayList<>();
+        	for (String w : weatherData.keySet()) {
+        		if (weatherData.get(w).temperatureF < max && weatherData.get(w).temperatureF > min) {
+        			data.add(w);
+        		}
+        	}
+        	 String msg = "Cities with temperatures between " + min + " and " + max + ": ";
+             for (String d : data) {
+             	msg += d + ", ";
+             }
+             JOptionPane.showMessageDialog(null, msg);
         }
     }
 }
--- src/_08_World_Clocks/WorldClocks.java
+++ src/_08_World_Clocks/WorldClocks.java
@@ -1,14 +1,19 @@
 package _08_World_Clocks;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.Timer;
 
 /*
@@ -35,58 +40,101 @@ import javax.swing.Timer;
  */
 
 public class WorldClocks implements ActionListener {
-    ClockUtilities clockUtil;
-    Timer timer;
-    TimeZone timeZone;
+	ClockUtilities clockUtil;
+	Timer timer;
+	String dateStr, timeStr;
+	TimeZone timeZone;
+	ArrayList<JTextArea> addedTextAreas;
+	JFrame frame;
+	JPanel panel;
+	ArrayList<JTextArea> textAreas;
+	JButton check;
+	JTextField cityField, countryField;
+	String city;
+	ArrayList<Calendar> times;
+	ArrayList<String> names;
+	public WorldClocks() {
+		times = new ArrayList<>();
+		clockUtil = new ClockUtilities();
+		textAreas = new ArrayList<>();
+		names = new ArrayList<>();
+		addedTextAreas = new ArrayList<>();
+		timer = new Timer(1000, this);
+		// The format for the city must be: city, country (all caps)
+		city = "San Diego, US";
+		TimeZone timeZone = clockUtil.getTimeZoneFromCityName(city);
+		Calendar c = Calendar.getInstance(timeZone);
+		times.add(c);
+		names.add(city);
+		JTextArea textArea1 = new JTextArea();
+		textAreas.add(textArea1);
 
-    JFrame frame;
-    JPanel panel;
-    JTextArea textArea;
-    
-    String city;
-    String dateStr;
-    String timeStr;
-    
-    public WorldClocks() {
-        clockUtil = new ClockUtilities();
+		// Sample starter program
+		frame = new JFrame();
+		panel = new JPanel();
+		cityField = new JTextField("City");
+		countryField = new JTextField("Country");
+		cityField.setPreferredSize(new Dimension(150, 30));
+		countryField.setPreferredSize(new Dimension(75, 30));
+		check = new JButton("Enter City");
+		check.addActionListener(this);
+		countryField.addActionListener(this);
+		cityField.addActionListener(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(100, 100);
+		frame.add(panel);
+		panel.add(cityField);
+		panel.add(countryField);
+		panel.add(check);
+		for (JTextArea textArea : textAreas) {
+			textArea = new JTextArea();
+			panel.add(textArea);
+		}
 
-        // The format for the city must be: city, country (all caps)
-        city = "Chicago, US";
-        timeZone = clockUtil.getTimeZoneFromCityName(city);
-        
-        Calendar calendar = Calendar.getInstance(timeZone);
-        String month = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
-        String dayOfWeek = calendar.getDisplayName( Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
-        dateStr = dayOfWeek + " " + month + " " + calendar.get(Calendar.DAY_OF_MONTH) + " " + calendar.get(Calendar.YEAR);
-        
-        System.out.println(dateStr);
+		// This Timer object is set to call the actionPerformed() method every
+		// 1000 milliseconds
+		timer = new Timer(1000, this);
+		timer.start();
+	}
 
-        // Sample starter program
-        frame = new JFrame();
-        panel = new JPanel();
-        textArea = new JTextArea();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setSize(100, 100);
-        frame.add(panel);
-        panel.add(textArea);
-        textArea.setText(city + "\n" + dateStr);
-        
-        // This Timer object is set to call the actionPerformed() method every
-        // 1000 milliseconds
-        timer = new Timer(1000, this);
-        timer.start();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent arg0) {
-        Calendar c = Calendar.getInstance(timeZone);
-        String militaryTime = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
-        String twelveHourTime = " [" + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND) + "]";
-        timeStr = militaryTime + twelveHourTime;
-        
-        System.out.println(timeStr);
-        textArea.setText(city + "\n" + dateStr + "\n" + timeStr);
-        frame.pack();
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == check) {
+			String city = cityField.getText() + ", " + countryField.getText();
+			TimeZone timeZone = clockUtil.getTimeZoneFromCityName(city);
+			Calendar c = Calendar.getInstance(timeZone);
+			String amOrPM;
+			if (c.AM_PM == c.AM)
+				amOrPM = "AM";
+			else
+				amOrPM = "PM";
+			String time = c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND) + " "
+					+ amOrPM;
+			times.add(c);
+			names.add(city);
+			textAreas.add(new JTextArea());
+			System.out.println("Time in " + city + ": " + time);
+		}
+		if (e.getSource() == timer) {
+			for (int i = 0; i < textAreas.size(); i++) {
+				System.out.println("TIMER");
+				Calendar c = times.get(i);
+				String militaryTime = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":"
+						+ c.get(Calendar.SECOND);
+				String twelveHourTime = " [" + c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":"
+						+ c.get(Calendar.SECOND) + "]";
+				dateStr = "";
+				timeStr = militaryTime + twelveHourTime;
+				JTextArea textArea = textAreas.get(i);
+				textAreas.get(i).setText(names.get(i) + '\n' + dateStr + '\n' + timeStr);
+				if (!addedTextAreas.contains(textArea)) {
+					panel.add(textArea);
+					addedTextAreas.add(textArea);
+				}
+				panel.add(textArea);
+			}
+			frame.pack();
+		}
+	}
 }
